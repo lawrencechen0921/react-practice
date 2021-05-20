e3a3edabfa7cae7e1af5518e87484530a8c46306
lawrencechen0921 <show99520show99520@gmail.com>
2021-05-21 01:51:26 +0800

    更改code配置

---
+++ src/pages/Home/index.js
@@ -0,0 +1,17 @@
+import Edit from './components/Edit'
+import List from './components/List'
+import './index.css'
+
+
+
+
+const Home = () => {
+    return <div className="app">
+        <Edit />
+        <List />
+    </div>
+
+}
+
+export default Home 
+
